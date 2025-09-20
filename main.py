26c0c374dcf518c01f9368eae50b67aebe2361e6
sababaradaran97 <sababaradaran97@gmail.com>
2025-09-20 15:13:09 +0330

    update 9 done

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from data_base.data_base_manager import *
 from view.view_program import main_view
 from tools.mudals import *
 
-craete_table()
+try:
+    craete_table()
 
-total_number = count_bottom() 
-list_of_expired_products = check_date()
-list_of_names = choose_title()
+    total_number = count_bottom() 
+    list_of_expired_products = check_date()
+    list_of_names = choose_title()
 
-main_view( list_of_expired_products , total_number , list_of_names )
+    main_view( list_of_expired_products , total_number , list_of_names )
 
+except Exception as e:
+    print ( "Error :" , type( e ) )
+    print ( "Message :" , e )
--- view/view_program.py
+++ view/view_program.py
@@ -11,48 +11,47 @@ def main_view( list_of_expired_products , total_number , list_of_names ) :
     window.title( "shop" )
     window.geometry ( "600x500" )
 
+
     def button_action():
         messagebox.showinfo( "", "Total Number of Products: " + str( total_number ) )
-        # Message ( text = "Total : " + str( total_number ) )
+        # Message ( text = "Total : " + str( total_number ) )        
+
+    def title_combo_eventhandler( event ) :
+
+        title_str = title.get()
+        title_str = str( title_str )
+        title_str = title_str.split( "-" )
+        id = title_str[ 0 ]
+
+        connection = sqlite3.connect( "data_base/data_base.db" )
+        result = connection.cursor().execute( f""" SELECT brand , price FROM product WHERE id = "{id}" """ ).fetchall()
+        connection.commit()
+        connection.close()
+
+        brand.set( result[ 0 ][ 0 ] )
+        price.set( result[ 0 ][ 1 ] )
+        
+        
 
     title = StringVar()
     Label( window , text = "product name" ).place( x = 25 , y = 30 )
-    combobox = Combobox( window , textvariable = title, values = list_of_names , width = 17 , state = "readonly" ).place( x = 120 , y = 30 )
-    title = title.get()
-
-    def information( title ) :
-
-        id , name = title.split( "-" )
-
-        connection = sqlite3.connect( "data_base/data_base.db" )
-        brand = connection.cursor().execute( f""" SELECT brand FROM product WHERE id = {id} """ ).fetchall()
-        price = connection.cursor().execute( f""" SELECT price FROM product WHERE id = {id} """ ).fetchall()
-        connection.close()
-
-        brand = brand[ 0 ][ 0 ]
-        price = price[ 0 ][ 0 ]
-
-        return brand , price
+    combobox = Combobox( window , textvariable = title, values = list_of_names , width = 17 , state = "readonly" )
+    combobox.bind('<<ComboboxSelected>>', title_combo_eventhandler)
+    combobox.place( x = 120 , y = 30 )    
     
-    result = information( title )
 
-    price = StringVar()
-    brand = StringVar()
-    
-    brand = result[ 0 ]
-    price = result[ 1 ]
-
-    
+   
+    brand = StringVar()    
     Label( window , text = "product brand" ).place( x = 25 , y = 70 )
     Entry( window , textvariable = brand , state = "readonly" ).place( x = 120 , y = 70 )
 
-
+    price = StringVar()
     Label( window , text = "product price" ).place( x = 25 , y = 110 )
     Entry( window , textvariable = price , width = 20 , state = "readonly" ).place( x = 120 , y = 110 )
 
 
     Label( window , text = "Expired date products" ).place( x = 360 , y = 30 )
-    Combobox( window , values = list_of_expired_products , width = 23 , state = "readonly" ).place( x = 360 , y = 70 )
+    Combobox( window , values = list_of_expired_products , width = 17 , state = "readonly" ).place( x = 360 , y = 70 )
 
 
     Button( 
@@ -62,5 +61,13 @@ def main_view( list_of_expired_products , total_number , list_of_names ) :
         command = button_action 
     ).place( x = 25 , y = 200 ) 
 
+    '''
+    result_as_list = information( title )
+    brand = result_as_list[ 0 ]
+    price = result_as_list[ 1 ]
+
+    print ( brand , price , result_as_list )
+    '''
 
     window.mainloop()
+
