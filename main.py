3c6e68dab54098abf0cf57cb8c819c885ec63325
sababaradaran97 <sababaradaran97@gmail.com>
2025-09-20 12:54:45 +0330

    update 6 done

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-try:
-    pass
+from data_base.data_base_manager import *
+from view.view_program import main_view
+from tools.mudals import *
 
-except:
-    pass
+# main_view --> 2 interence --> count_bottom , check_date from data_base.data_base_manager
+
+
+total_number = count_bottom() 
+list_of_expired_products = check_date()
+list_of_names = choose_title()
+# main_view( list_of_expired_products , total_number , list_of_names )
+
